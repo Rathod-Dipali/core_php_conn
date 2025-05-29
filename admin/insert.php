b40c460f92e990dbf92f63e464cbd554ba341bb9
Rathod-Dipali <dipalir236@gmail.com>
2025-05-29 15:44:36 +0530

    exsist emailk

--- admin/insert.php
+++ admin/insert.php
@@ -7,10 +7,18 @@ $mobile = $_POST['mobile'];
 $password = $_POST['password'];
 $image_name = $_FILES['image']['name'];
 
-$path = "../assets/upload/".$image_name;
+$path = "../assets/upload/" . $image_name;
 $tmp_file = $_FILES['image']['tmp_name'];
-move_uploaded_file($tmp_file,$path);
+move_uploaded_file($tmp_file, $path);
 
+$check_query = "select * from `admin` where email='$email' limit 1";
+$check_result = mysqli_query($con, $check_query);
+$check_row = mysqli_fetch_assoc($check_result);
+if (isset($check_row)) {
+    echo "<script>alert('This email is alredy exsist.')</script>";
+    echo "<script>window.location='add.php'</script>";
+}else{
+    
 $query = "INSERT INTO `admin` (name, email, mobile,password,image) VALUES ('$name', '$email', '$mobile','$password','$image_name')";
 
 if (mysqli_query($con, $query)) {
@@ -20,3 +28,6 @@ if (mysqli_query($con, $query)) {
     echo "<script>alert('Not Registred')</script>";
     echo "<script>window.location='index.php'</script>";
 }
+
+}
+?>
